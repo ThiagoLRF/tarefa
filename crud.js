354fdbbc4d26a67b955ad808488a82681fbc7b2f
ThiagoLRF <thiagoleandro.1702@gmail.com>
2023-05-09 20:20:38 -0300

    ok

--- crud.js
+++ crud.js
@@ -17,6 +17,10 @@ function cadastrar(){
 
 }
 
+function deletarCard(botao){
+    botao.parentNode.parentNode.parentNode.remove()
+}
+
 function gerarCard(tarefa){
     return `<div class="col-12 col-md-6 col-lg-3">
     <div class="card">
@@ -32,7 +36,7 @@ function gerarCard(tarefa){
             <a href="#" class="btn btn-success">
                 <i class="bi bi-check-lg"></i>
             </a>
-            <a href="#" class="btn btn-danger">
+            <a href="#" onClick="deletarCard(this)" class="btn btn-danger">
                 <i class="bi bi-trash"></i>
             </a>
         </div>
