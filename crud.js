3bf62a97aad79fa729891779fa7b9191f048a072
ThiagoLRF <thiagoleandro.1702@gmail.com>
2023-05-16 22:48:12 -0300

    ok

--- crud.js
+++ crud.js
@@ -3,12 +3,28 @@ document.querySelector("#salvar").addEventListener("click", cadastrar)
 let lista_tarefas =[]
 
 window.addEventListener("load", ()=> {
-    lista_tarefas = JSON.parse(localStorage.getItem("lista_tarefas"))
-    lista_tarefas.forEach((tarefa) => {
-        document.querySelector("#tarefas").innerHTML += gerarCard(tarefa)
+    lista_tarefas = JSON.parse(localStorage.getItem("lista_tarefas")) || []
+   atualizar()
 
     })
-})
+
+    document.querySelector("#pendentes").addEventListener("click", () => {
+        lista_tarefas = lista_tarefas.filter(tarefa => !tarefa.concluida)
+        atualizar()
+    })
+
+    document.querySelector(#"busca").addEventListener("keyup",() => {
+        const titulo = document.querySelector(#"busca").value
+        lista_tarefas =
+            lista_tarefas.filter(tarefa => tarefa.titulo.includes(titulo))
+            atualizar()
+    })
+
+    document.querySelector("#concluidas").addEventListener("click", () => {
+        lista_tarefas = lista_tarefas.filter(tarefa => tarefa.concluida)
+        atualizar()
+    })
+
 
 function cadastrar(){
     const modal = bootstrap.Modal.getInstance(document.querySelector("#exampleModal"))
@@ -18,10 +34,12 @@ function cadastrar(){
     let categoria = document.querySelector("#categoria").value
 
     const tarefa = {
-        titulo: titulo,
-        descricao: descricao,
-        notas: notas,
-        categoria: categoria,
+        id: Date.now(),
+        titulo,
+        descricao,
+        notas,
+        categoria,
+        concluida: false
     }
 
     if (tarefa.titulo.length == 0){
@@ -33,17 +51,37 @@ function cadastrar(){
 
     document.querySelector("#tarefas").innerHTML += gerarCard(tarefa)
 
-    localStorage.setItem("lista_tarefas", JSON.stringify (lista_tarefas))
-
+    
+    salvar()
     modal.hide()
 
+} 
+
+function atualizar(){
+    document.querySelector("#tarefas").innerHTML = ""
+    lista_tarefas.forEach((tarefa) => {
+        document.querySelector("#tarefas").innerHTML += gerarCard(tarefa)
+})}
+
+function salvar(){
+    localStorage.setItem("lista_tarefas", JSON.stringify (lista_tarefas))
 }
 
-function deletarCard(botao){
-    botao.parentNode.parentNode.parentNode.remove()
+function deletarCard(id){
+  lista_tarefas = lista_tarefas.filter((tarefa) => tarefa.id != id) //arrow function
+  salvar()
+  atualizar()
+}
+
+function concluir(id){
+    let tarefa_encotrada = lista_tarefas.find((tarefa) => tarefa.id == ids)
+    tarefa_encotrada.concluida = true
+    salvar()
+    atualizar()
 }
 
 function gerarCard(tarefa){
+    const disabled = (tarefa.concluida) ? "disabled" : ""
     return `<div class="col-12 col-md-6 col-lg-3">
     <div class="card">
         <div class="card-header">
@@ -55,10 +93,10 @@ function gerarCard(tarefa){
                 <span class="badge text-bg-warning">${tarefa.categoria}</span>
             </p>
             <p class="card-text">${tarefa.notas}</p>
-            <a href="#" class="btn btn-success">
+            <a href="#" onClick="concluir(${tarefa.id})" class="btn btn-success ${disabled}">
                 <i class="bi bi-check-lg"></i>
             </a>
-            <a href="#" onClick="deletarCard(this)" class="btn btn-danger">
+            <a href="#" onClick="deletarCard(${tarefa.id})" class="btn btn-danger">
                 <i class="bi bi-trash"></i>
             </a>
         </div>
